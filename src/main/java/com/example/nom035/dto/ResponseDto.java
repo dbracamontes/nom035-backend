cff4f4059c5e45b784c95fdee984523bf7a2b3eb
dbracamontes <danielbracamontes87@gmail.com>
2025-10-21 13:53:24 -0600

    Revert "PR-5 Add missing responseDto"
    This reverts commit 1d2dcdd00011a362be0405523bf6e8067c86de1c.

--- src/main/java/com/example/nom035/dto/ResponseDto.java
+++ src/main/java/com/example/nom035/dto/ResponseDto.java
@@ -1,34 +1,84 @@
 package com.example.nom035.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ResponseDto {
+    
+    @JsonProperty("id")
     private Long id;
+    
+    @JsonProperty("surveyApplicationId")
     private Long surveyApplicationId;
+    
+    @JsonProperty("questionId") 
     private Long questionId;
+    
+    @JsonProperty("optionAnswerId")
     private Long optionAnswerId;
-    private String freeText;
-
+    
+    @JsonProperty("textAnswer")
+    private String textAnswer;
+    
+    // Constructors
     public ResponseDto() {}
-
-    public ResponseDto(Long id, Long surveyApplicationId, Long questionId, Long optionAnswerId, String freeText) {
+    
+    public ResponseDto(Long id, Long surveyApplicationId, Long questionId, Long optionAnswerId, String textAnswer) {
         this.id = id;
         this.surveyApplicationId = surveyApplicationId;
         this.questionId = questionId;
         this.optionAnswerId = optionAnswerId;
-        this.freeText = freeText;
+        this.textAnswer = textAnswer;
     }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public Long getSurveyApplicationId() { return surveyApplicationId; }
-    public void setSurveyApplicationId(Long surveyApplicationId) { this.surveyApplicationId = surveyApplicationId; }
-
-    public Long getQuestionId() { return questionId; }
-    public void setQuestionId(Long questionId) { this.questionId = questionId; }
-
-    public Long getOptionAnswerId() { return optionAnswerId; }
-    public void setOptionAnswerId(Long optionAnswerId) { this.optionAnswerId = optionAnswerId; }
-
-    public String getFreeText() { return freeText; }
-    public void setFreeText(String freeText) { this.freeText = freeText; }
+    
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Long getSurveyApplicationId() {
+        return surveyApplicationId;
+    }
+    
+    public void setSurveyApplicationId(Long surveyApplicationId) {
+        this.surveyApplicationId = surveyApplicationId;
+    }
+    
+    public Long getQuestionId() {
+        return questionId;
+    }
+    
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+    
+    public Long getOptionAnswerId() {
+        return optionAnswerId;
+    }
+    
+    public void setOptionAnswerId(Long optionAnswerId) {
+        this.optionAnswerId = optionAnswerId;
+    }
+    
+    public String getTextAnswer() {
+        return textAnswer;
+    }
+    
+    public void setTextAnswer(String textAnswer) {
+        this.textAnswer = textAnswer;
+    }
+    
+    @Override
+    public String toString() {
+        return "ResponseDto{" +
+                "id=" + id +
+                ", surveyApplicationId=" + surveyApplicationId +
+                ", questionId=" + questionId +
+                ", optionAnswerId=" + optionAnswerId +
+                ", textAnswer='" + textAnswer + '\'' +
+                '}';
+    }
 }
